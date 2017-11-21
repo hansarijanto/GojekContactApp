7b6bacab662cc1970eba5fc56b77c2a6b14f4762
Hans Arijanto <hansarijanto@hotmail.com>
2017-11-21 17:07:31 +0700

    Initial Commit

---
+++ GojekContactsAppUITests/GojekContactsAppUITests.swift
@@ -0,0 +1,36 @@
+//
+//  GojekContactsAppUITests.swift
+//  GojekContactsAppUITests
+//
+//  Created by Hans Arijanto on 11/21/17.
+//  Copyright © 2017 Hans Arijanto. All rights reserved.
+//
+
+import XCTest
+
+class GojekContactsAppUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
