04ec24657baefc99fc4deefb4d9baa4d74865d25
Hans Arijanto <hansarijanto@hotmail.com>
2017-11-26 01:45:44 +0700

    add x button when editing field

--- GojekContactsApp/ContactDetailViewController.swift
+++ GojekContactsApp/ContactDetailViewController.swift
@@ -38,10 +38,12 @@ class ContactDetailIconButton: UIButton {
     }
 }
 
-class ContactDetailTableCellView: UITableViewCell {
+class ContactDetailTableCellView: UITableViewCell, UITextFieldDelegate {
     
     public let mainLabel: UILabel = UILabel()
     public let contentField: UITextField = UITextField()
+    private let cancelButton: UIButton = UIButton()
+    private let cancelImage: UIImageView = UIImageView()
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -56,8 +58,17 @@ class ContactDetailTableCellView: UITableViewCell {
         let contentFont: UIFont = UIFont.systemFont(ofSize: 16.0)
         self.contentField.font = contentFont
         
+        self.cancelImage.image = UIImage(named: "x")
+        self.cancelButton.addTarget(self, action: #selector(ContactDetailTableCellView.didTapCancelButton), for: .touchUpInside)
+        
+        self.hideCancelButton(hide: true)
+        
         self.contentView.addSubview(self.mainLabel)
         self.contentView.addSubview(self.contentField)
+        self.contentView.addSubview(self.cancelImage)
+        self.contentView.addSubview(self.cancelButton)
+        
+        self.contentField.delegate = self
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -72,14 +83,42 @@ class ContactDetailTableCellView: UITableViewCell {
         self.mainLabel.autoPinEdge(.left, to: .left, of: self.contentView, withOffset: 0.0)
         self.mainLabel.autoPinEdge(.right, to: .left, of: self.contentView, withOffset: 100.0)
         
+        self.cancelImage.autoSetDimensions(to: CGSize(width: 15.0, height: 15.0))
+        self.cancelImage.autoAlignAxis(toSuperviewAxis: .horizontal)
+        self.cancelImage.autoPinEdge(.right, to: .right, of: self.contentView, withOffset: -15.0)
+        
+        self.cancelButton.autoPinEdge(toSuperviewEdge: .top)
+        self.cancelButton.autoPinEdge(toSuperviewEdge: .bottom)
+        self.cancelButton.autoPinEdge(toSuperviewEdge: .right)
+        self.cancelButton.autoSetDimension(.width, toSize: 45.0)
+        
         self.contentField.autoPinEdge(toSuperviewEdge: .top)
         self.contentField.autoPinEdge(toSuperviewEdge: .bottom)
         self.contentField.autoPinEdge(.left, to: .right, of: self.mainLabel, withOffset: 32.0)
-        self.contentField.autoPinEdge(toSuperviewEdge: .right)
+        self.contentField.autoPinEdge(.right, to: .left, of: self.cancelButton, withOffset: 0.0)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        self.hideCancelButton(hide: true)
+    }
+    
+    @objc public func didTapCancelButton() {
+        self.contentField.text = ""
+    }
+    
+    public func hideCancelButton(hide: Bool) {
+        self.cancelImage.isHidden = hide
+        self.cancelButton.isHidden = hide
+    }
+    
+    // MARK: UITextField Delegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.hideCancelButton(hide: false)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.hideCancelButton(hide: true)
     }
 }
 
