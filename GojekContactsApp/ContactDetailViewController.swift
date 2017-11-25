5f2dd2f7bb9698107383bf1a26bf5b6b1a3b6fa6
Hans Arijanto <hansarijanto@hotmail.com>
2017-11-25 23:05:46 +0700

    hide detail table rows when vieweing and is empty, show all only when editing

--- GojekContactsApp/ContactDetailViewController.swift
+++ GojekContactsApp/ContactDetailViewController.swift
@@ -312,6 +312,8 @@ class ContactDetailViewController: UIViewController, UITableViewDelegate, UITabl
                 self.tableView.isUserInteractionEnabled = false
             }
         }
+        
+        self.tableView.reloadData()
     }
     
     // MARK: Icon Touch Callbacks
@@ -363,7 +365,32 @@ class ContactDetailViewController: UIViewController, UITableViewDelegate, UITabl
     
     // MARK: Table View Datasource
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 58.0
+        let height: CGFloat = 58.0
+        
+        
+        if self.mode == .view {
+            // only return height if the contact info on the row is not nil, otherwise hide it by having 0.00 height
+            if indexPath.row == 0 && self.contact.firstName != nil {
+                return height
+            }
+            
+            if indexPath.row == 1 && self.contact.lastName != nil {
+                return height
+            }
+            
+            if indexPath.row == 2 && self.contact.mobile != nil {
+                return height
+            }
+            
+            if indexPath.row == 3 && self.contact.email != nil {
+                return height
+            }
+            
+            return 0.0
+        } else {
+            // if editing show all
+            return height
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
